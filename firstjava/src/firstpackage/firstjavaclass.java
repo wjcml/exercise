6ba6fc465df076f9f8b1bb237a60f83ccb8c5aff
wjcml <37792055+wjcml@users.noreply.github.com>
2019-09-03 14:49:09 +0800

    20190903

---
+++ firstjava/src/firstpackage/firstjavaclass.java
@@ -0,0 +1,9 @@
+package firstpackage;
+
+public class firstjavaclass {
+	public static void main(String []args) {
+        System.out.println("Hello World,sir"); // ¥Ú”° Hello World
+        System.out.println("sfa");
+        System.out.println("faks");
+    }
+}
